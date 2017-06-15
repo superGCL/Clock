a4daf79d2ee6a5b40f9c31be4734927d5028d234
supergcl <983944994@qq.com>
2017-06-15 17:14:31 +0800

    first commit

---
+++ Clock/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Clock
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel_clock = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel_clock.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel_clock
+            // 
+            this.panel_clock.Controls.Add(this.pictureBox1);
+            this.panel_clock.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_clock.Location = new System.Drawing.Point(0, 0);
+            this.panel_clock.Name = "panel_clock";
+            this.panel_clock.Size = new System.Drawing.Size(408, 262);
+            this.panel_clock.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(408, 262);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.SizeChanged += new System.EventHandler(this.pictureBox1_SizeChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 262);
+            this.Controls.Add(this.panel_clock);
+            this.DoubleBuffered = true;
+            this.Name = "mainForm";
+            this.Text = "Clock";
+            this.panel_clock.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_clock;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Clock/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class mainForm : Form
+    {
+        private Bitmap buffer;
+        public mainForm()
+        {
+            InitializeComponent();
+
+            init();
+        }
+
+        Pen borderPen = new Pen(Brushes.Black);//圆形边框画笔
+        Pen secondScalePen = new Pen(Brushes.Black);//秒刻度画笔
+        Pen minuteScalePen = new Pen(Brushes.Black);//分刻度画笔
+        Pen hourPen = new Pen(Brushes.Red);  //时针画笔
+        Pen minutePen = new Pen(Brushes.Blue);//分针画笔
+        Pen secondPen = new Pen(Brushes.Black);//秒针画笔
+        int borderGap = 5;
+        Font font = new System.Drawing.Font(FontFamily.GenericSerif, 16);
+        int panelWidth = 0;
+        int panelHeight = 0;
+        int rdius = 0;
+
+        private void init()
+        {
+            buffer = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = buffer;
+            borderPen.Width = 2;
+            minuteScalePen.Width = 3;//分刻度线要比秒刻度线粗
+            hourPen.Width = 5;                   //时针宽度
+            minutePen.Width = 3;                  //分针宽度
+            panelWidth = panel_clock.Width;
+            panelHeight = panel_clock.Height;
+            //绘制的时钟是圆形的，因此选择较短的边作为直径
+            rdius = (panelWidth < panelHeight ? panelWidth : panelHeight) / 2;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (buffer == null) return;
+
+            Graphics g = Graphics.FromImage(buffer);
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            
+            //绘制前，先清空内容
+            g.Clear(Color.White);
+
+            //先绘制时钟的边框(双边框)
+            g.DrawEllipse(borderPen, (panelWidth / 2 - rdius + borderGap), 5, ((rdius - borderGap) * 2), ((rdius - borderGap) * 2));
+            g.DrawEllipse(borderPen, (panelWidth / 2 - rdius), 0, rdius * 2, rdius * 2);
+
+            //画圆心
+            g.FillEllipse(Brushes.Black, (panelWidth-10) / 2, (panelHeight - 10) / 2, 10, 10);
+
+            //画秒刻度 60根 每一秒一根刻度线
+            for (int i = 0; i < 60; i++)
+            {
+                //绘出刻度线
+                g.DrawLine(secondScalePen, panelWidth / 2, borderGap, panelWidth / 2, rdius / 30 + borderGap);
+
+                //旋转角度6度
+                g.TranslateTransform(panelWidth / 2, panelHeight / 2); //设置旋转中心为圆心
+                g.RotateTransform(6); //旋转
+                g.TranslateTransform(-panelWidth / 2, -panelHeight / 2);  //设置回原来的中心
+            }
+
+            for (int i = 0; i < 60; i += 5)
+            {
+                //绘出刻度线
+                g.DrawLine(minuteScalePen, panelWidth / 2, borderGap, panelWidth / 2, rdius / 30 + borderGap + 5);
+
+                //绘出时间
+                SizeF size = g.MeasureString(("" + i / 5) == "0" ? "12" : ("" + i / 5), font);
+                g.DrawString(("" + i / 5) == "0" ? "12" : ("" + i / 5), font, Brushes.Black, (panelWidth - size.Width) / 2, rdius / 30 + borderGap + 5);
+
+                //旋转角度30度
+                g.TranslateTransform(panelWidth / 2, panelHeight / 2);
+                g.RotateTransform(30);
+                g.TranslateTransform(-panelWidth / 2, -panelHeight / 2);
+            }
+
+            //画时针
+            int hour = DateTime.Now.Hour;
+            float angle = (hour % 12) / 12.0f * 360;
+
+            //旋转角度至合适的位置，以便画出时针
+            g.TranslateTransform(panelWidth / 2, panelHeight / 2);
+            g.RotateTransform(angle);
+            g.TranslateTransform(-panelWidth / 2, -panelHeight / 2);
+
+            //开始画时针
+            g.DrawLine(hourPen, panelWidth / 2, panelHeight / 2, panelWidth / 2, rdius / 3);
+
+            //再旋转回来
+            g.TranslateTransform(panelWidth / 2, panelHeight / 2);
+            g.RotateTransform(-angle);
+            g.TranslateTransform(-panelWidth / 2, -panelHeight / 2);
+
+            //画分针
+            int minute = DateTime.Now.Minute;
+            angle = (minute % 60) / 60.0f * 360;
+
+            //旋转角度至合适的位置，以便画出分针
+            g.TranslateTransform(panelWidth / 2, panelHeight / 2);
+            g.RotateTransform(angle);
+            g.TranslateTransform(-panelWidth / 2, -panelHeight / 2);
+
+            //开始画分针
+            g.DrawLine(minutePen, panelWidth / 2, panelHeight / 2, panelWidth / 2, borderGap + 3);
+
+            //再旋转回来
+            g.TranslateTransform(panelWidth / 2, panelHeight / 2);
+            g.RotateTransform(-angle);
+            g.TranslateTransform(-panelWidth / 2, -panelHeight / 2);
+
+            //画秒针
+            int second = DateTime.Now.Second;
+            angle = (second % 60) / 60.0f * 360;
+
+            //旋转角度至合适的位置，以便画出秒针
+            g.TranslateTransform(panelWidth / 2, panelHeight / 2);
+            g.RotateTransform(angle);
+            g.TranslateTransform(-panelWidth / 2, -panelHeight / 2);
+
+            //开始画秒针
+            g.DrawLine(secondPen, panelWidth / 2, panelHeight / 2, panelWidth / 2, borderGap + 3);
+
+            //再旋转回来
+            g.TranslateTransform(panelWidth / 2, panelHeight / 2);
+            g.RotateTransform(-angle);
+            g.TranslateTransform(-panelWidth / 2, -panelHeight / 2);
+
+            pictureBox1.Image = buffer;
+        }
+
+        private void pictureBox1_SizeChanged(object sender, EventArgs e)
+        {
+            if(buffer != null)
+            {
+                buffer.Dispose();
+                buffer = null;
+            }
+
+            init();
+        }
+    }
+}
